46e20c80d7e8ac888dc686c48ad19cd139e1d479
Talia Fabregas <talia.fabregas@mail.utoronto.ca>
2024-01-23 20:15:44 +0000

    data cleaning

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,10 +1,11 @@
 #### Preamble ####
-# Purpose: Tests
+# Purpose: Tests if data is within the relevant time period
+# and area
 # Author: Talia Fabregas
 # Date: 18 January 2024
 # Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: Run 01-download_data and 02-data_cleaning first
+# Pre-requisites: Run 02-data_cleaning first
 
 #### Workspace setup ####
 library(tidyverse)
@@ -12,3 +13,4 @@ library(dplyr)
 library(janitor)
 
 #### Test data ####
+shooting_data <- read_csv("outputs/data/toronto_shooting_data.csv")
